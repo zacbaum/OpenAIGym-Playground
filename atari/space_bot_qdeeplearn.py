fb447fa5a86cf236dc49ada957a7880e9578acc7
Zachary Baum <12zmcb@queensu.ca>
2019-11-05 14:18:09 +0000

    Deep QLearning Reinforcement Bots for Atari Games

---
+++ atari/space_bot_qdeeplearn.py
@@ -0,0 +1,46 @@
+"""
+Space Invaders DeepQLearn - Fully featured deep q-learning network (using pre-trained model).
+"""
+
+import cv2
+import gym
+import numpy as np
+import random
+import tensorflow as tf
+from bot_6_a3c import a3c_model
+random.seed(0)  # make results reproducible
+tf.set_random_seed(0)
+
+num_episodes = 10
+
+
+def downsample(state):
+    return cv2.resize(state, (84, 84), interpolation=cv2.INTER_LINEAR)[None]
+
+def main():
+    env = gym.make('SpaceInvaders-v0')  # create the game
+    env.seed(0)  # make results reproducible
+    rewards = []
+
+    model = a3c_model(load='models/SpaceInvaders-v0.tfmodel')
+    for _ in range(num_episodes):
+        episode_reward = 0
+        states = [downsample(env.reset())]
+        while True:
+            if len(states) < 4:
+                action = env.action_space.sample()
+            else:
+                frames = np.concatenate(states[-4:], axis=3)
+                action = np.argmax(model([frames]))
+            state, reward, done, _ = env.step(action)
+            states.append(downsample(state))
+            episode_reward += reward
+            if done:
+                print('Reward: %d' % episode_reward)
+                rewards.append(episode_reward)
+                break
+    print('Average reward: %.2f' % (sum(rewards) / len(rewards)))
+
+
+if __name__ == '__main__':
+    main()
